9aef642084a0adbac90cf4a95b019611e214a8da
kashipu <graficowm@gmail.com>
2025-02-10 16:57:38 -0500

    Añadido output and server

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+  server: {
+    host: '0.0.0.0',
+  }
 });
