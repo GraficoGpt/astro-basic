158504a2bbbff501266007c03e558e5aa276eb0c
kashipu <graficowm@gmail.com>
2025-02-10 17:12:54 -0500

    añadido server and start script

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
 // https://astro.build/config
-export default defineConfig({
+export default defineConfig( {
   output: 'server',
-  adapter: node({
+  adapter: node( {
     mode: 'standalone'
-  }),
-});
+  } ),
+  server: {
+    host: '0.0.0.0'
+  }
+} );
